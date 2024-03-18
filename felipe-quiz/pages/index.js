f53e070cceee795fc755a58ae732dfa98c672187
Zomer Silveira <felipe.silveira@hp.com>
2024-03-18 15:10:52 -0300

    adding proptypes and creating input and button components

--- felipe-quiz/pages/index.js
+++ felipe-quiz/pages/index.js
@@ -8,6 +8,7 @@ import QuizLogo from "../src/components/QuizLogo";
 import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
+import Button from "../src/components/Button"
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -47,19 +48,15 @@ export default function Home() {
                 console.log("Fazendo uma submissão por meio do react");
               }}
             >
-              <input
-                onChange={function (infosDoEvento) {
-                  console.log(infosDoEvento.target.value);
-                  // State
-                  // name = infosDoEvento.target.value;
-                  setName(infosDoEvento.target.value);
-                }}
-                placeholder="Diga seu nome"
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
